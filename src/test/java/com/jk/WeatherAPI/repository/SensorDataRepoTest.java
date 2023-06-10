ba0fddc5bb0fd7dbd1021db48736a6168c6b2035
John Kelly <kellyjra@gmail.com>
2023-06-10 22:31:33 +0100

    chore: some additional cleanup

--- src/test/java/com/jk/WeatherAPI/repository/SensorDataRepoTest.java
+++ src/test/java/com/jk/WeatherAPI/repository/SensorDataRepoTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
@@ -33,47 +32,5 @@ public class SensorDataRepoTest {
         Assertions.assertTrue(result.isEmpty());
     }
 
-    /*
-    @Test
-    public void testFindAll(){
-        SensorData sensorData1 = new SensorData();
-        sensorData1.setSampleId(111L);
-
-        SensorData sensorData2 = new SensorData();
-        sensorData1.setSampleId(222L);
-
-        sensorDataRepo.save(sensorData1);
-        sensorDataRepo.save(sensorData2);
-
-        List<SensorData> result = sensorDataRepo.findAll();
-        Assertions.assertTrue(result.size() == 2);
-    }*/
-
-    /*
-    @Test
-    public void testGetAverageTemperatureBySensorIdsAndDateRange() {
-        // Create sample SensorData entities and save them
-        SensorData sensorData1 = new SensorData();
-        sensorData1.setSampleId(111L);
-        sensorData1.setSensorId(1L);
-        sensorData1.setTemperature(25.5);
-        sensorData1.setSampleTime(new Timestamp(new Date().getTime()));
-        sensorDataRepo.save(sensorData1);
-
-        SensorData sensorData2 = new SensorData();
-        sensorData2.setSampleId(222L);
-        sensorData2.setSensorId(1L);
-        sensorData2.setTemperature(30.0);
-        sensorData2.setSampleTime(new Timestamp(new Date().getTime()));
-        sensorDataRepo.save(sensorData2);
-
-        // Call the getAverageTemperatureBySensorIdsAndDateRange method
-        List<Long> sensorIds = Arrays.asList(1L);
-        Date startDate = new Date();
-        Date endDate = new Date();
-        Double averageTemperature = sensorDataRepo.getAverageTemperatureBySensorIdsAndDateRange(sensorIds, startDate, endDate);
-
-        // Assert that the average temperature is calculated correctly
-        Assertions.assertEquals(27.75, averageTemperature);
-    }*/
+    //TODO Add lots more tests here!
 }
--- src/test/java/com/jk/WeatherAPI/util/TestUtils.java
+++ src/test/java/com/jk/WeatherAPI/util/TestUtils.java
@@ -25,26 +25,56 @@ public class TestUtils {
     }
 
     public SensorDataDTO generateSensorDataDTO(final Long sampleId, final Long sensorId) {
-        List<MetricDTO> metrics = generateMetricsDTO();
+        final List<MetricDTO> metrics = generateMetricsDTO();
         return new SensorDataDTO(sampleId, sensorId, metrics);
     }
 
     public SensorDataDTO generateInvalidSensorDataDTO(final Long sampleId, final Long sensorId) {
-        List<MetricDTO> metrics = generateInvalidMetricsDTO();
+        final List<MetricDTO> metrics = generateInvalidMetricsDTO();
         return new SensorDataDTO(sampleId, sensorId, metrics);
     }
 
     private List<MetricDTO> generateInvalidMetricsDTO() {
-        List<MetricDTO> metrics = new ArrayList<>();
+        final List<MetricDTO> metrics = new ArrayList<>();
         metrics.add(new MetricDTO(null, 10.0));
         metrics.add(new MetricDTO(MetricType.HUMIDITY, null));
         return metrics;
     }
 
     private List<MetricDTO> generateMetricsDTO() {
-        List<MetricDTO> metrics = new ArrayList<>();
+        final List<MetricDTO> metrics = new ArrayList<>();
         metrics.add(new MetricDTO(MetricType.TEMPERATURE, 10.0));
         metrics.add(new MetricDTO(MetricType.HUMIDITY, 20.0));
+        return metrics;
+    }
+
+    public StatsQueryRequestDTO generateStatsQueryRequestDTO(final List<Long> sensorIds, final StatType statsType, final Boolean searchAllSensors, final boolean queryTemp, final boolean queryRain, final boolean queryWind, final boolean queryHum, final String startDate, final String endDate){
+
+        final List<MetricType> metricTypes = new ArrayList<>();
+        if(queryHum){
+            metricTypes.add(MetricType.HUMIDITY);
+        }
+        if(queryRain){
+            metricTypes.add(MetricType.RAINFALL);
+        }
+        if(queryWind){
+            metricTypes.add(MetricType.WINDSPEED);
+        }
+        if(queryTemp){
+            metricTypes.add(MetricType.TEMPERATURE);
+        }
+        return new StatsQueryRequestDTO(sensorIds, metricTypes, statsType, startDate, endDate, searchAllSensors);
+    }
+
+    public SensorDataDTO generateSensorDataDTOForUpdate(final Long sampleId, final Long sensorId) {
+        final List<MetricDTO> metrics = generateMetricsDTOForUpdate();
+        return new SensorDataDTO(sampleId, sensorId, metrics);
+    }
+
+    private List<MetricDTO> generateMetricsDTOForUpdate() {
+        final List<MetricDTO> metrics = new ArrayList<>();
+        metrics.add(new MetricDTO(MetricType.TEMPERATURE, 18.0));
+        metrics.add(new MetricDTO(MetricType.RAINFALL, 6.0));
         return metrics;
     }
 
